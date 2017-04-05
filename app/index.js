b6ad96465d593763688785d2be56ca8a07461c93
Stephanie Winn <stephanie.n.winn@gmail.com>
2017-04-04 21:48:37 -0500

    add emojis to the console

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
 import 'whatwg-fetch';
+const commands = [
+  {
+    emoji: '🦄',
+    name: 'unicorn'
+  }, {
+    emoji: '✨',
+    name: 'sparkles'
+  }
+];
+
+{
+  commands.forEach(({ name, emoji }) => window.console[name] = (...args) => console.log(`${emoji} ${args.join(', ')}`));
+}
+
+console.unicorn('Hi! Thanks for taking a look at my work. I am currently seeking full-time employment.');
+console.sparkles('I would love to be your next employee! Reach me at stephanie.n.winn@gmail.com. Talk soon!');
